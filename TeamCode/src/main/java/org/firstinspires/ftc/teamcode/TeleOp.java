c1384864994f5984298101279bce57e626a15071
Naty <darkwounds.james@gmail.com>
2024-12-09 18:58:57 +0200

    IntoTheDeep10.1

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -0,0 +1,61 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.gamepad.ButtonReader;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.gamepad.GamepadKeys;
+import com.arcrobotics.ftclib.gamepad.TriggerReader;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp(name="Robot16Aug", group="Linear Opmode")
+public class TeleOp extends GlobalScope
+{
+    private ElapsedTime runtime = new ElapsedTime();
+
+    public void runOpMode()
+    {
+
+        Initialise();
+
+        waitForStart();
+
+        Controler();
+
+        while (opModeIsActive())
+        {
+            MiscareBaza();
+            SliderExtend();
+            SliderBaza();
+            Roteste();
+            Cleste();
+            Intake();
+            Outake();
+            telemetry.update();
+            telemetry.addData("Stanga ", BazaStanga.getPosition());
+            telemetry.addData("Dreapta ", BazaDreapta.getPosition());
+            telemetry.addData("Gheara", ServoGhearaIntake.getPosition());
+            telemetry.addData("inst", IntakeStanga.getPosition());
+            telemetry.addData("indr", IntakeDreapta.getPosition());
+            RotireStanga.readValue();
+            RotireDreapta.readValue();
+            if(RotireStanga.wasJustPressed()){
+                BazaDreapta.setPosition(BazaDreapta.getPosition() + 0.001);
+                BazaStanga.setPosition(BazaStanga.getPosition() + 0.001);
+            }
+            if(RotireDreapta.wasJustPressed()){
+                BazaStanga.setPosition(BazaStanga.getPosition() - 0.001);
+                BazaDreapta.setPosition(BazaDreapta.getPosition() - 0.001);
+            }
+            /**IntakeSus.readValue();
+            IntakeJos.readValue();
+            if(IntakeSus.wasJustPressed()){
+                BazaDreapta.setPosition(BazaDreapta.getPosition() + 0.01);
+                BazaStanga.setPosition(BazaStanga.getPosition() + 0.01);
+            }
+            if(IntakeJos.wasJustPressed()){
+                BazaDreapta.setPosition(BazaDreapta.getPosition() - 0.01);
+                BazaStanga.setPosition(BazaStanga.getPosition() - 0.01);
+            }*/
+        }
+    }
+}

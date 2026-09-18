d7d7b1a4e1556e0f02216e097f4c0294fb236a2d
Rares 2787 <raresgospidin27@gmail.com>
2026-09-18 14:28:42 +0300

    Reparare Subsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystem.java
@@ -3,8 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public interface Subsystem {
-
-    public void Initialize();
-    public void LinkComponents(HardwareMap hardwareMap);
+    public void LinkComponents(HardwareMap hwMap);
+    public void Initialize(HardwareMap hwMap);
     public void Run();
 }
